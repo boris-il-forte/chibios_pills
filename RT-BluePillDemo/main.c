4c37df2c3730ecd1d2a122748c40aaafb77e13e6
Boris_il_forte <boris.ilpossente@hotmail.it>
2020-08-23 23:02:42 +0200

    Added blue pill demo
    - Added blue pill demo
    - Added gitignore

---
+++ RT-BluePillDemo/main.c
@@ -0,0 +1,26 @@
+#include "ch.h"
+#include "hal.h"
+
+static THD_WORKING_AREA(waThread1, 0);
+static THD_FUNCTION(Thread1, arg) {
+  (void)arg;
+  chRegSetThreadName("blinker");
+
+  palSetPadMode(GPIOC, GPIOC_LED, PAL_MODE_OUTPUT_OPENDRAIN); // only 50MHz in GPIOv1
+  while (true) {
+    palTogglePad(GPIOC, GPIOC_LED);
+    chThdSleepMilliseconds(500);
+  }
+}
+
+int main(void) {
+  halInit();
+  chSysInit();
+  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO+1, Thread1, NULL);
+
+  while (true) {
+    chThdSleepSeconds(1);
+  }
+
+  return 0;
+}
